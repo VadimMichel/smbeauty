4bc85c451e61e60e824dac6ce5798e20fe33c379
VadimMichel <vadimmichel5@gmail.com>
2025-12-04 07:26:34 +0100

    feat: create project structure and integrate fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./components/navbar";
+import Navbar from "./components/navbar/navbar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

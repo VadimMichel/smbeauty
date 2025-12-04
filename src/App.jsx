67aea72db781e5a5a6adc8ef40d557a5d55450e3
VadimMichel <vadimmichel5@gmail.com>
2025-12-04 21:05:30 +0100

    feat: reorganize project structure with index.js and App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from "./components/navbar/navbar";
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <Navbar />
+        );
+    }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./components/navbar/navbar";
+import App from "./App";
 import "./main.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Navbar />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );

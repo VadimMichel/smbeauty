9f152f758db8ceb68f6d440656101d065fd617c0
VadimMichel <vadimmichel5@gmail.com>
2025-12-04 19:54:19 +0100

    feat: add HTML and CSS for Navbar component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import "./navbar.css"
+import logo from "../../assets/icons/logo-white.png"
 
 class Navbar extends Component {
     state = {  } 
     render() { 
         return (
             <nav>
-                <img src="/" alt="" />
-                <ul>
-                    <li><a href="">Behandlungen</a></li>
-                    <li><a href="">Salon</a></li>
-                    <li><a href="">Über Uns</a></li>
-                    <li><a href="">Kontakt</a></li>
-                </ul>
+                <div>
+                    <img src={logo} alt="logo" />
+                    <ul>
+                        <li><a href="">Behandlungen</a></li>
+                        <li><a href="">Salon</a></li>
+                        <li><a href="">Über Uns</a></li>
+                        <li><a href="">Kontakt</a></li>
+                    </ul>
+                </div>
             </nav>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/navbar/navbar";
+import "./main.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
